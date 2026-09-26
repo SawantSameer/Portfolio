fdfd44842b2b16655fe890a6c27129b0ee45052f
Sameer Sawant <sameersawant@Sameers-MacBook-Air.local>
2026-09-26 13:26:59 +0530

    Now at the point of rendering home page

--- main/urls.py
+++ main/urls.py
@@ -1,4 +1,9 @@
 from django.urls import path
 from . import views
 
-urlpatterns = []
+urlpatterns = [
+    path("", views.home, name = "home"),
+    path("home/", views.home, name = "home"),
+    path("contact/", views.contact, name = "contact"),
+    path("project/<int:id>/", views.project, name = "project")
+]
